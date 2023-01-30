ea1cbb773db45dd8e06d9bf8aa0dd76737c8c779
petthebeaver <madplanet31@gmail.com>
2023-01-30 19:33:51 +0300

    some init msg

--- example.py
+++ example.py
@@ -1,18 +1,25 @@
-import tkinter
+import tkinter as tk
 import tkinter.messagebox
 import customtkinter
 import numpy as np
 from patterns_init import pics
+from PIL import ImageTk
+import projection_func as pf
 
 data_freq_red, data_freq_green = pics()
-
 
 customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
 customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
 freqs = data_freq_red.columns
+
 check_array = list(np.zeros(len(freqs)))
 button_array = list(np.zeros(len(freqs)))
+images = {}
+
+#images = pf.set_freq(data_freq_red, images, check_array)
+
+
 
 class App(customtkinter.CTk):
     def __init__(self):
@@ -31,7 +38,8 @@ class App(customtkinter.CTk):
         self.sidebar_frame = customtkinter.CTkFrame(self, width=140, corner_radius=0)
         self.sidebar_frame.grid(row=0, column=0, rowspan=4, sticky="nsew")
         self.sidebar_frame.grid_rowconfigure(4, weight=1)
-        self.logo_label = customtkinter.CTkLabel(self.sidebar_frame, text="CustomTkinter", font=customtkinter.CTkFont(size=20, weight="bold"))
+        self.logo_label = customtkinter.CTkLabel(self.sidebar_frame, text="CustomTkinter",
+                                                 font=customtkinter.CTkFont(size=20, weight="bold"))
         self.logo_label.grid(row=0, column=0, padx=20, pady=(20, 10))
         self.sidebar_button_1 = customtkinter.CTkButton(self.sidebar_frame, command=self.sidebar_button_event)
         self.sidebar_button_1.grid(row=1, column=0, padx=20, pady=10)
@@ -46,7 +54,8 @@ class App(customtkinter.CTk):
         # self.appearance_mode_optionemenu.grid(row=6, column=0, padx=20, pady=(10, 10))
         self.scaling_label = customtkinter.CTkLabel(self.sidebar_frame, text="UI Scaling:", anchor="w")
         self.scaling_label.grid(row=7, column=0, padx=20, pady=(10, 0))
-        self.scaling_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["80%", "90%", "100%", "110%", "120%"],
+        self.scaling_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame,
+                                                               values=["80%", "90%", "100%", "110%", "120%"],
                                                                command=self.change_scaling_event)
         self.scaling_optionemenu.grid(row=8, column=0, padx=20, pady=(10, 20))
 
@@ -54,12 +63,39 @@ class App(customtkinter.CTk):
         self.entry = customtkinter.CTkEntry(self, placeholder_text="CTkEntry")
         self.entry.grid(row=3, column=1, columnspan=2, padx=(20, 0), pady=(20, 20), sticky="nsew")
 
-        self.main_button_1 = customtkinter.CTkButton(master=self, fg_color="transparent", border_width=2, text_color=("gray10", "#DCE4EE"))
+        self.main_button_1 = customtkinter.CTkButton(master=self, fg_color="transparent", border_width=2,
+                                                     text_color=("gray10", "#DCE4EE"))
         self.main_button_1.grid(row=3, column=3, padx=(20, 20), pady=(20, 20), sticky="nsew")
 
+
+
+
+
+
+
         # create textbox
-       # self.textbox = customtkinter.CTkTextbox(self, width=250)
-       # self.textbox.grid(row=0, column=1, padx=(20, 0), pady=(20, 0), sticky="nsew")
+
+
+        #main_window = tk.Toplevel(self.sidebar_frame)
+        img = customtkinter.CTkImage(data_freq_red['000'][0], size=(500, 500))
+        self.pattern_window = customtkinter.CTkLabel(self, image=img, text='')
+
+
+        self.pattern_window.grid(row=0, column=1, padx=(20, 0), pady=(20, 0))
+        cols = data_freq_red.columns
+
+        for i in cols[5:10]:
+
+            for k in range(6):
+                img = customtkinter.CTkImage(data_freq_red[i].iloc[k], size=(500, 500))
+                #self.pattern_window['image'] = img
+                self.pattern_window.after(150, self.pattern_window.configure(image=img, text=''))
+
+
+
+
+
+
 
         # create tabview
         self.tabview = customtkinter.CTkTabview(self, width=250)
@@ -88,11 +124,14 @@ class App(customtkinter.CTk):
         self.radio_var = tkinter.IntVar(value=0)
         self.label_radio_group = customtkinter.CTkLabel(master=self.radiobutton_frame, text="CTkRadioButton Group:")
         self.label_radio_group.grid(row=0, column=2, columnspan=1, padx=10, pady=10, sticky="")
-        self.radio_button_1 = customtkinter.CTkRadioButton(master=self.radiobutton_frame, variable=self.radio_var, value=0)
+        self.radio_button_1 = customtkinter.CTkRadioButton(master=self.radiobutton_frame, variable=self.radio_var,
+                                                           value=0)
         self.radio_button_1.grid(row=1, column=2, pady=10, padx=20, sticky="n")
-        self.radio_button_2 = customtkinter.CTkRadioButton(master=self.radiobutton_frame, variable=self.radio_var, value=1)
+        self.radio_button_2 = customtkinter.CTkRadioButton(master=self.radiobutton_frame, variable=self.radio_var,
+                                                           value=1)
         self.radio_button_2.grid(row=2, column=2, pady=10, padx=20, sticky="n")
-        self.radio_button_3 = customtkinter.CTkRadioButton(master=self.radiobutton_frame, variable=self.radio_var, value=2)
+        self.radio_button_3 = customtkinter.CTkRadioButton(master=self.radiobutton_frame, variable=self.radio_var,
+                                                           value=2)
         self.radio_button_3.grid(row=3, column=2, pady=10, padx=20, sticky="n")
 
         # create checkbox and switch frame
@@ -102,28 +141,29 @@ class App(customtkinter.CTk):
         self.checkbox_1.grid(row=1, column=0, pady=(20, 10), padx=20, sticky="n")
         self.checkbox_2 = customtkinter.CTkCheckBox(master=self.checkbox_slider_frame)
         self.checkbox_2.grid(row=2, column=0, pady=10, padx=20, sticky="n")
-        self.switch_1 = customtkinter.CTkSwitch(master=self.checkbox_slider_frame, command=lambda: print("switch 1 toggle"))
+        self.switch_1 = customtkinter.CTkSwitch(master=self.checkbox_slider_frame,
+                                                command=lambda: print("switch 1 toggle"))
         self.switch_1.grid(row=3, column=0, pady=10, padx=20, sticky="n")
         self.switch_2 = customtkinter.CTkSwitch(master=self.checkbox_slider_frame)
         self.switch_2.grid(row=4, column=0, pady=(10, 20), padx=20, sticky="n")
 
         # create slider and progressbar frame
-        self.slider_progressbar_frame = customtkinter.CTkFrame(self, fg_color="transparent")
-        self.slider_progressbar_frame.grid(row=1, column=1, columnspan=2, padx=(20, 0), pady=(20, 0), sticky="nsew")
-        self.slider_progressbar_frame.grid_columnconfigure(0, weight=1)
-        self.slider_progressbar_frame.grid_rowconfigure(4, weight=1)
-        self.seg_button_1 = customtkinter.CTkSegmentedButton(self.slider_progressbar_frame)
-        self.seg_button_1.grid(row=0, column=0, padx=(20, 10), pady=(10, 10), sticky="ew")
-        self.progressbar_1 = customtkinter.CTkProgressBar(self.slider_progressbar_frame)
-        self.progressbar_1.grid(row=1, column=0, padx=(20, 10), pady=(10, 10), sticky="ew")
-        self.progressbar_2 = customtkinter.CTkProgressBar(self.slider_progressbar_frame)
-        self.progressbar_2.grid(row=2, column=0, padx=(20, 10), pady=(10, 10), sticky="ew")
-        self.slider_1 = customtkinter.CTkSlider(self.slider_progressbar_frame, from_=0, to=1, number_of_steps=4)
-        self.slider_1.grid(row=3, column=0, padx=(20, 10), pady=(10, 10), sticky="ew")
-        self.slider_2 = customtkinter.CTkSlider(self.slider_progressbar_frame, orientation="vertical")
-        self.slider_2.grid(row=0, column=1, rowspan=5, padx=(10, 10), pady=(10, 10), sticky="ns")
-        self.progressbar_3 = customtkinter.CTkProgressBar(self.slider_progressbar_frame, orientation="vertical")
-        self.progressbar_3.grid(row=0, column=2, rowspan=5, padx=(10, 20), pady=(10, 10), sticky="ns")
+        # self.slider_progressbar_frame = customtkinter.CTkFrame(self, fg_color="transparent")
+        # self.slider_progressbar_frame.grid(row=1, column=1, columnspan=2, padx=(20, 0), pady=(20, 0), sticky="nsew")
+        # self.slider_progressbar_frame.grid_columnconfigure(0, weight=1)
+        # self.slider_progressbar_frame.grid_rowconfigure(4, weight=1)
+        # self.seg_button_1 = customtkinter.CTkSegmentedButton(self.slider_progressbar_frame)
+        # self.seg_button_1.grid(row=0, column=0, padx=(20, 10), pady=(10, 10), sticky="ew")
+        # self.progressbar_1 = customtkinter.CTkProgressBar(self.slider_progressbar_frame)
+        # self.progressbar_1.grid(row=1, column=0, padx=(20, 10), pady=(10, 10), sticky="ew")
+        # self.progressbar_2 = customtkinter.CTkProgressBar(self.slider_progressbar_frame)
+        # self.progressbar_2.grid(row=2, column=0, padx=(20, 10), pady=(10, 10), sticky="ew")
+        # self.slider_1 = customtkinter.CTkSlider(self.slider_progressbar_frame, from_=0, to=1, number_of_steps=4)
+        # self.slider_1.grid(row=3, column=0, padx=(20, 10), pady=(10, 10), sticky="ew")
+        # self.slider_2 = customtkinter.CTkSlider(self.slider_progressbar_frame, orientation="vertical")
+        # self.slider_2.grid(row=0, column=1, rowspan=5, padx=(10, 10), pady=(10, 10), sticky="ns")
+        # self.progressbar_3 = customtkinter.CTkProgressBar(self.slider_progressbar_frame, orientation="vertical")
+        # self.progressbar_3.grid(row=0, column=2, rowspan=5, padx=(10, 20), pady=(10, 10), sticky="ns")
 
         # set default values
         self.sidebar_button_3.configure(state="disabled", text="Disabled CTkButton")
@@ -136,13 +176,13 @@ class App(customtkinter.CTk):
         self.scaling_optionemenu.set("100%")
         self.optionmenu_1.set("CTkOptionmenu")
         self.combobox_1.set("CTkComboBox")
-        self.slider_1.configure(command=self.progressbar_2.set)
-        self.slider_2.configure(command=self.progressbar_3.set)
-        self.progressbar_1.configure(mode="indeterminnate")
-        self.progressbar_1.start()
-       #.insert("0.0", "CTkTextbox\n\n" + "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.\n\n" * 20)
-        self.seg_button_1.configure(values=["CTkSegmentedButton", "Value 2", "Value 3"])
-        self.seg_button_1.set("Value 2")
+        # self.slider_1.configure(command=self.progressbar_2.set)
+        # self.slider_2.configure(command=self.progressbar_3.set)
+        # self.progressbar_1.configure(mode="indeterminnate")
+        # self.progressbar_1.start()
+        # .insert("0.0", "CTkTextbox\n\n" + "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.\n\n" * 20)
+        # self.seg_button_1.configure(values=["CTkSegmentedButton", "Value 2", "Value 3"])
+        # self.seg_button_1.set("Value 2")
 
     def open_input_dialog_event(self):
         dialog = customtkinter.CTkInputDialog(text="Type in a number:", title="CTkInputDialog")
--- projection_func.py
+++ projection_func.py
@@ -1,22 +1,25 @@
-def set_array(check_array, button_array):
+import tkinter as tk
+from PIL import ImageTk
 
+
+def set_array(check_array, button_array, data_freq_red, images, base):
     for i in range(len(check_array)):
-        check_array[i] = BooleanVar()
+        check_array[i] = tk.BooleanVar()
 
     for i in range(2, len(button_array) - 1):
-        button_array[i] = Checkbutton(b, text=data_freq_red.columns[i],
-                                      variable=check_array[i],
-                                      onvalue=1,
-                                      offvalue=0,
-                                      height=2,
-                                      width=5)
+
+        button_array[i] = tk.Checkbutton(base, text=data_freq_red.columns[i],
+                                         variable=check_array[i],
+                                         onvalue=1,
+                                         offvalue=0,
+                                         height=2,
+                                         width=5)
 
         # if i!=0 and  i in [1,2,3,4,5,6,7,8,9,11,13,15,18,19,20,21,22]:
         if i != 0 and i in [5, 10, 22]:
-            # if i!=0 and  i in [1,2,3,5,22]:
+
             button_array[i].select()
-
-
+    return button_array
 
 
 def sel(data_freq, data_freq_red, data_freq_green):
@@ -33,7 +36,7 @@ def set_all(pattern_window):
 
     set_freq()
     keys = list(images)
-    pattern_window.config(image=images2['000'])
+    pattern_window.config(image=images['000'])
     pattern_window.update()
 
 
@@ -48,7 +51,7 @@ def start(start_button, images, main_window, pattern_window, flag):
         main_window.after(150, pattern_window.update())
 
     pattern_window.config(image=images['000'])
-    # photo_button['background']='green'
+
     if flag:
         # flag=False
         sel()
@@ -56,11 +59,12 @@ def start(start_button, images, main_window, pattern_window, flag):
 
 
 def set_freq(data_freq, images, check_array):
-    cols = data_freq.columns
 
+    cols = data_freq.columns
     images['000'] = ImageTk.PhotoImage(data_freq['000'][0])
     for i, j in enumerate(check_array):
-        if j.get():
+        # or j.get()
+        if True:
             for k in range(6):
                 images[cols[i] + '_' + str(k)] = ImageTk.PhotoImage(data_freq.T.iloc[i][k])
 
